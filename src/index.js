30a4c6fa98244d51ca54832033e28fb40032a339
IvettaDashkova <ivettazakonno@gmail.com>
2021-01-15 03:38:46 +0200

    делает основное дз

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
+import './styles.css';
+import inputSearchTpl from './templates/inputSearch.hbs';
+import imageTpl from './templates/imageTpl.hbs';
+import apiService from './js/apiService';
 
+const input = inputSearchTpl();
+document.body.insertAdjacentHTML('afterbegin', input);
+
+const body = document.body;
+const searchForm = document.querySelector('.search-form');
+const loadMoreBtn = document.querySelector('[data-action="load-more"]');
+
+const listImages = document.createElement('ul');
+listImages.classList.add('gallery');
+body.append(listImages);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  const form = e.currentTarget;
+  apiService.query = form.elements.query.value;
+
+    listImages.innerHTML = '';
+  
+
+  apiService.resetPage();
+  apiService.fetchImages().then(dataImages => {
+    updateListImages(dataImages);
+  });
+}
+
+function updateListImages(dataImages) {
+  const contentImages = imageTpl(dataImages);
+  listImages.insertAdjacentHTML('beforeend', contentImages);
+}
+
+searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.addEventListener('click', () => {
+  apiService.fetchImages().then(dataImages => {
+    updateListImages(dataImages);
+    window.scrollTo({
+        top: document.documentElement.offsetHeight,
+        behavior: 'smooth'
+    });
+  });
+});
---
+++ src/js/apiService.js
@@ -0,0 +1,26 @@
+const apiKey = '19891525-b410a0a37a9fe6964038d322b';
+export default {
+  searchQuery: '',
+  page: 1,
+  fetchImages() {
+    const BASE_URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.query}&page=${this.page}&per_page=12&key=${apiKey}`;
+
+    return fetch(BASE_URL)
+      .then(response => response.json())
+      .then(({ hits }) => {
+        this.page += 1;
+        return hits;
+      })
+        .catch(error => console.log(error))
+     
+  },
+  resetPage() {
+    this.page = 1;
+  },
+  get query() {
+    return this.searchQuery;
+  },
+  set query(value) {
+    this.searchQuery = value;
+  },
+};
